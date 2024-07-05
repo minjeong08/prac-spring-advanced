4738db6e41aab2bd3bc35daec6dbaaaac5e65b47
minnjung0887921 <minjeong0887921@gmail.com>
2024-07-05 15:58:38 +0900

    템플릿 메서드 패턴 - 예제3

--- advanced/src/test/java/hello/advanced/app/trace/template/TemplateMethodTest.java
+++ advanced/src/test/java/hello/advanced/app/trace/template/TemplateMethodTest.java
@@ -9,6 +9,30 @@ import org.junit.jupiter.api.Test;
 @Slf4j
 public class TemplateMethodTest {
 
+    /**
+     * 템플릿 메서드 패턴, 익명 내부 클래스 사용
+     */
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1 = {}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("클래스 이름2 = {}", template2.getClass());
+        template1.execute();
+    }
+
     @Test
     void templateMethodV1() {
         AbstractTemplate template1 = new SubClassLogic1();
