5e05930602cc8058beaef3f94cbddef9218c7cff
minnjung0887921 <minjeong0887921@gmail.com>
2024-07-05 15:55:48 +0900

    템플릿 메서드 패턴 - 예제2

--- advanced/src/test/java/hello/advanced/app/trace/template/TemplateMethodTest.java
+++ advanced/src/test/java/hello/advanced/app/trace/template/TemplateMethodTest.java
@@ -1,10 +1,23 @@
 package hello.advanced.app.trace.template;
 
+import hello.advanced.app.trace.template.code.AbstractTemplate;
+import hello.advanced.app.trace.template.code.SubClassLogic1;
+import hello.advanced.app.trace.template.code.SubClassLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+
+    }
 
     @Test
     void templateMethodV0() {
