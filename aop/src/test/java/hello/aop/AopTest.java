b436ccc8ec3543d43bbbf096ba2199e439d1c977
minnjung0887921 <minjeong0887921@gmail.com>
2024-07-11 15:10:28 +0900

    스프링 AOP 구현 1

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -2,14 +2,18 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
+@Import(AspectV1.class)
 @SpringBootTest
 public class AopTest {
 
@@ -32,7 +36,7 @@ public class AopTest {
 
     @Test
     void exception() {
-        Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
+        assertThatThrownBy(() -> orderService.orderItem("ex"))
                 .isInstanceOf(IllegalStateException.class);
     }
 }
